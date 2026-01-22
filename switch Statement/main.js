470070242c0fe82e5854f210f4bdf652380c7a11
Dhegnaas <abdirahman460930@gmail.com>
2026-01-22 15:33:13 +0300

    switchStatement

---
+++ switch Statement/main.js
@@ -0,0 +1,15 @@
+const expr = 'botato';
+
+switch(expr){
+    case "Apple":
+        console.log("appless are $2");
+        break;
+        case "mangoes" : 
+        console.log("magoes are $1");
+        break;
+        case "banana": 
+        console.log("banana are $0.5")
+        break;
+        default :
+        console.log(" maheyno dalaabka " + expr)
+}
