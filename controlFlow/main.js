b3890857d70f32fe8caca4778c007b2387d291ef
Dhegnaas <abdirahman460930@gmail.com>
2026-01-22 15:15:49 +0300

    second commit

---
+++ controlFlow/main.js
@@ -0,0 +1,11 @@
+let per =81;
+
+if (per >90){
+    console.log("A+");
+}else if (per >80){
+    console.log("B+")
+}else if (per > 70 ){
+    console.log("C+")
+}else {
+    console.log("haray!")  
+}
