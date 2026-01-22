ffda3ca8dbd78fe12495ed21758c67c300143c71
Dhegnaas <abdirahman460930@gmail.com>
2026-01-22 14:39:14 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+//arithmetic operators
+
+// let x = 5;
+// let y = 10;
+
+// let result = x + y;  
+
+// console.log(result);
+
+
+// == equal / lamid ah
+// != not equal / lamid maahan
+// === strict equal / data type ka iyo value ga inow is ku mid noqdo
+// !== strict not equal / data type ka iyo value ga ineyna is ku mid noqdo
+// > greater than / ka weyn 
+// >= greater than or equal / ka weyn ama lamid ah
+// < less than / ka yar
+// <=less than or equal / ka yar ama ka weyn 
+
+
+// logical operators
+
+// logic AND (&&) t / T = true markaan bis ee true soo bexee inta kale false waaye
+// logical or (||) f /f =false Kaliya marka labaduba FALSE yihiin ayay FALSE noqoneysaa
+// logical not (!) Haddii shardi uu yahay true, ! wuxuu ka dhigaa false // Haddii shardi uu yahay false, ! wuxuu ka dhigaa true
+
+
+// comparison operators
+let var1 = true && true;
+let var2 = false || false;
+let var3 = !false;
+
+console.log( var1 );
+console.log( var2 );
+console.log( var3 );
+
+
+
+
+
